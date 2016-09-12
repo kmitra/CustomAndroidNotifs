8c7945babb4aa6ff2233a0b8f583391ee9307974
Kristian Mitra <kristian.m@pocketplaylab.com>
2016-09-12 20:44:10 +0800

    Added code to test local notifications using the custom layout

--- app/src/main/java/com/example/tutorial/customandroidnotifs/MainActivity.java
+++ app/src/main/java/com/example/tutorial/customandroidnotifs/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.tutorial.customandroidnotifs;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,38 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createTestNotification();
+    }
+
+    private void createTestNotification() {
+
+        int icon = R.drawable.icon192x192;
+        long when = System.currentTimeMillis();
+
+        Notification notification = new NotificationCompat.Builder(this.getApplicationContext())
+                .setSmallIcon(icon)
+                .setContentText("Custom Notification")
+                .setWhen(when)
+                .build();
+
+        NotificationManager mNotificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        RemoteViews contentView = new RemoteViews(getPackageName(), R.layout.custom_push_layout);
+        contentView.setImageViewResource(R.id.image, R.drawable.icon192x192);
+        contentView.setTextViewText(R.id.title, "Custom notification");
+        contentView.setTextViewText(R.id.text, "This is a custom layout");
+        notification.contentView = contentView;
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+        notification.contentIntent = contentIntent;
+
+        notification.flags |= Notification.FLAG_NO_CLEAR; //Do not clear the notification
+        notification.defaults |= Notification.DEFAULT_LIGHTS; // LED
+        notification.defaults |= Notification.DEFAULT_VIBRATE; //Vibration
+        notification.defaults |= Notification.DEFAULT_SOUND; // Sound
+
+        mNotificationManager.notify(1, notification);
     }
 }
