930c261e6d43c4dd3bddb7d1bd4f76344218294f
Kristian Mitra <kristian.m@pocketplaylab.com>
2016-09-18 22:18:53 +0800

    Initially comment out code in order to figure out external libraries

--- CustomNotifsLib/src/main/java/ppl/unity/cubeslots/androidnotifs/CustomNotifs.java
+++ CustomNotifsLib/src/main/java/ppl/unity/cubeslots/androidnotifs/CustomNotifs.java
@@ -10,31 +10,31 @@ import android.content.Intent;
  */
 public class CustomNotifs {
 
-    public int scheduleNotification(long secondsFromNow, String title, String subtitle, String tickerText, String data, String smallIcon, String largeIcon, int requestCode)
-    {
-        Activity context = getActivity();
-        Intent intent = new Intent(context, AlarmManagerReceiver.class);
-        intent.putExtra("title", title);
-        intent.putExtra("subtitle", subtitle);
-        intent.putExtra("tickerText", tickerText);
-        intent.putExtra("data", data);
-
-        if ((smallIcon != null) && (smallIcon != "")) {
-            intent.putExtra("smallIcon", smallIcon);
-        }
-        if ((largeIcon != null) && (largeIcon != "")) {
-            intent.putExtra("largeIcon", largeIcon);
-        }
-        intent.putExtra("contextClassName", getActivity().getClass().getName());
-        intent.putExtra("requestCode", requestCode);
-        intent.putExtra("vibrate", 1);
-        intent.putExtra("sound", 1);
-
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 134217728);
-
-        AlarmManager alarmManager = (AlarmManager)context.getSystemService("alarm");
-        alarmManager.set(1, System.currentTimeMillis() + secondsFromNow * 1000L, pendingIntent);
-
-        return requestCode;
-    }
+//    public int scheduleNotification(long secondsFromNow, String title, String subtitle, String tickerText, String data, String smallIcon, String largeIcon, int requestCode)
+//    {
+//        Activity context = getActivity();
+//        Intent intent = new Intent(context, AlarmManagerReceiver.class);
+//        intent.putExtra("title", title);
+//        intent.putExtra("subtitle", subtitle);
+//        intent.putExtra("tickerText", tickerText);
+//        intent.putExtra("data", data);
+//
+//        if ((smallIcon != null) && (smallIcon != "")) {
+//            intent.putExtra("smallIcon", smallIcon);
+//        }
+//        if ((largeIcon != null) && (largeIcon != "")) {
+//            intent.putExtra("largeIcon", largeIcon);
+//        }
+//        intent.putExtra("contextClassName", getActivity().getClass().getName());
+//        intent.putExtra("requestCode", requestCode);
+//        intent.putExtra("vibrate", 1);
+//        intent.putExtra("sound", 1);
+//
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, requestCode, intent, 134217728);
+//
+//        AlarmManager alarmManager = (AlarmManager)context.getSystemService("alarm");
+//        alarmManager.set(1, System.currentTimeMillis() + secondsFromNow * 1000L, pendingIntent);
+//
+//        return requestCode;
+//    }
 }
